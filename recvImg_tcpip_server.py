c3bfe033a8ca4cac0f0ea716efdcade26a657ae5
JinSeon,Oh <46018569+ohjinjin@users.noreply.github.com>
2020-07-28 21:08:55 +0900

    Update recvImg_tcpip_server.py

--- recvImg_tcpip_server.py
+++ recvImg_tcpip_server.py
@@ -4,14 +4,17 @@ import cv2
 import numpy
 
 # socket 수신 버퍼를 읽어서 반환하는 함수
-def recvall(sock, count):
+def recvall(sock):
     buf = b''
-    while count:
+    flag = True
+    while flag:
         newbuf = sock.recv(count)
-        if not newbuf: return None
-        buf += nuewbuf
-        count -= len(newbuf)
-    print(buf)
+        if not newbuf:
+            flag = False
+            continue
+
+        buf += newbuf
+
     return buf
 
 # 수신에 사용될 server ip와 server port번호
@@ -28,17 +31,18 @@ print("연결 성공")
 
 # 편의를 위해 이미지의 길이를 16으로 지정하고 데이터를 수신하는 것
 # str 형의 이미지를 수신받아서 이미지로 변환하고 화면에 출력
-length = recvall(conn, 16)
-print(length)
+recvbytes = recvall(conn)
+print(len(recvbytes))
 
-stringData = recvall(conn, int(length))
-print(stringData)
-data = numpy.fromstring(stringData,dtype='uint8')
-print(data)
+nparr = numpy.fromstring(recvbytes, numpy.uint8)
+
+#for _ in nparr:
+#    print(_,end=' ')
 
 s.close()
-decimg = cv2.imdecode(data,1)
-#decimg = data.reshape(480,640,3)
+
+decimg = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
 cv2.imshow('SERVER',decimg)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
